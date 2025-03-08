1c55f55531b1247f864a245ee7fff49726b273c9
HiPrabel <prabel397@gmail.com>
2025-03-08 19:52:30 +0530

    authorization middleware added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+// when res is not used in method it can be replaced with '_'
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+  try {
+    // assigned it at login 
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    // console.log(token);
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+    // while generating access token _id is assigned 
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+     
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token")
+    }
+    
+    // if user is found assign new user object to req just like body, files
+    req.user = user;
+    next()
+  } catch (error) {
+
+    throw new ApiError(401, error?.message || "Invalid access token")
+  }
+
+})
